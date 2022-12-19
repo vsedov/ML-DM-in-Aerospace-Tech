fcfb12df6159a382ff24cdef62a71ab1e8ddc4f0
vsedov <viv.sedov@hotmail.com>
2022-12-19 17:04:42 +0000

    Chapter 1: Tensor Classifier

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ML-DM-in-aerospace-tech
+
 Notes on Machine Learning and data mining in aerospace technology.
+
+> These are not notes persay, but more so concepts that i was some what confused by which required me to get a further
+understanding on. This book has some rather interesting concepts which i had not come across.
+
+If you have some general machine learning knowledge this should be a nice little guidance or little further information
+regarding what this is on about.
+
+The book is referencing :
+>Aboul Ella Hassanien
+> Ashraf Darwish
+> Hesham El-Askary Editors
+> Machine Learning and Data Mining
+> in Aerospace Technology
---
+++ code/parafac.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorly as tl
+import tensorly.decomposition
+from icecream import ic
+from tabulate import tabulate
+
+np.random.seed(0)
+
+n = 2
+# Generate fake data
+data = np.random.rand(2, 2, n)
+# print(tabulate(data, tablefmt="latex", floatfmt=".2f"))
+
+print("Random Data is :\n", data)
+print()
+# decompose the tensor using PARAFAC
+# An Extension of PCA: Every component has a score and loading for each mode that it goes with
+for i in range(1, n + 1):
+    print(f"Rank : {i}")
+    print()
+    print([factor for factor in tl.decomposition.parafac(data, rank=i)])
+    print()
+
+# Invited in 1970 : https://en.wikipedia.org/wiki/Parafac
